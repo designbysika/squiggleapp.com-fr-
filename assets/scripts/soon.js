6e1d288f121491bccb1afe9405bda88e741146f9
Starr <shocked1370@gmail.com>
2024-11-04 22:04:39 -0600

    More file transfers

---
+++ assets/scripts/soon.js
@@ -0,0 +1,8 @@
+var soonAnimation = bodymovin.loadAnimation({
+    container: document.getElementById('soonAnimWrapper'),
+    path: 'assets/animations/landing/soon.json',
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    name: "soon",
+});
